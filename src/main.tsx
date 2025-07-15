5f7e4f943c92c76bde8452c70acf5aa5e3f85303
buathier.tom <buathier_tom@orange.fr>
2025-07-15 15:07:33 +0200

    fix: clerk key

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,20 @@ import { ClerkProvider } from '@clerk/clerk-react'
 import App from './App.tsx'
 import './index.css'
 
+interface ImportMetaEnv {
+  VITE_CLERK_PUBLISHABLE_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare global {
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+}
+
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
